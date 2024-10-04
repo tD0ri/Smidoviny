9c3c3e7acaaa2474da01ca4a05fc48213eea9fd5
dorii <153084559+tD0ri@users.noreply.github.com>
2024-10-04 19:09:16 +0200

    Add files via upload
    Věci z hodiny 4.10.24

---
+++ interface_Auto/Servant.java
@@ -0,0 +1,19 @@
+
+public class Servant
+{
+
+    public Servant()
+    {
+
+    }
+    
+    public int getPocetCestujicich(ICestujici vozidlo)
+    {
+        return vozidlo.getpocetCestujicich();
+    }
+    
+    public void priberCestujiciho(ICestujici vozidlo)
+    {
+        vozidlo.setPocetCestujicich(vozidlo.getpocetCestujicich()+1);
+    }
+}
